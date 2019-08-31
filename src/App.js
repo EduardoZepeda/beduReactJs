237105514615d03b55bd856e262850161d7b8e79
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2019-08-31 12:14:19 -0500

    Add functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,89 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function App() {
-  return <h1>Hello world</h1>
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      value: '',
+      todos: []
+    }
+  }
+
+  saveTodo = () => {
+    if(this.state.value.trim()){
+      this.setState({
+        value:'',
+        todos: [...this.state.todos, this.state.value.trim()]
+      })
+    }
+  }
+
+  updateValue = event =>{
+    this.setState({value: event.target.value})
+  }
+
+  render(){
+    const listItems = this.state.todos.map((element, index)=>{
+      return(
+        <ListItem button key={index}>
+          <Checkbox/>
+          <ListItemText primary={element}/>
+          <ListItemSecondaryAction>
+            <IconButton>
+              <DeleteIcon/>
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      )
+    })
+    return(
+      <React.Fragment>
+        <Typography variant="h2" align="center" gutterBottom>Todo list</Typography>
+        <Grid container justify="center">
+          <Grid item>
+            <form onSubmit={e=>{
+              e.preventDefault()
+              this.saveTodo()
+            }}>
+              <TextField
+                type = "text"
+                placeholder="Add to do"
+                value = {this.state.value}
+                onChange = {this.updateValue}
+                />
+              </form>
+          </Grid>
+        </Grid>
+        <Grid container justify="center">
+          <Grid item md={8}>
+          <List>
+            <ListItem button>
+              <Checkbox/>
+              <ListItemText primary="Check the prework!!!"/>
+              <ListItemSecondaryAction>
+                <IconButton>
+                  <DeleteIcon/>
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+            {listItems}
+          </List>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
